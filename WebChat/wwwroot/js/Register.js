128a0fe902c316145ae68f77bde50ad18d7cd9e3
User <User@VLADPC>
2020-06-09 23:30:39 +0300

    add jqwery plagin for validation ,fix validation form (html,css,js)

---
+++ WebChat/wwwroot/js/Register.js
@@ -0,0 +1,28 @@
+﻿
+$(function () {
+    $('form[name="callback"]').validate({
+        rules: {
+            name: 'required',
+            password: 'required',
+            confirmpassword: {
+                equalTo: "#password",
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+
+        },
+        messages: {
+            name: 'Enter Name',
+            password: 'Enter Password',
+            email: 'Entrer correct E-mail',
+            confirmpassword: 'Do not mach',
+
+        },
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+});
